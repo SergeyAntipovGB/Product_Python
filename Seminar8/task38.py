16a1ee97136d4c95c67283dc1420631ae08695f0
Sergey Antipov <ser_ok@mail.ru>
2023-06-15 00:28:30 +0300

    ended phone book for Product

--- Seminar8/task38.py
+++ Seminar8/task38.py
@@ -1,14 +1,3 @@
-'''
-Задача No(49) 38.
-Решение в группах Создать телефонный справочник с возможностью импорта и
-экспорта данных в формате .txt. Фамилия, имя, отчество, номер телефона -
-данные, которые должны находиться в файле.
-1. Программа должна выводить данные
-2. Программа должна сохранять данные в текстовом файле
-3. Пользователь может ввести одну из характеристик для поиска определенной
-записи(Например имя или фамилию человека)
-4. Использование функций. Ваша программа не должна быть линейной
-'''
 import os
 
 def show_menu():
@@ -21,8 +10,9 @@ def show_menu():
           "3. Найти абонента по фамилии\n"
           "4. Найти абонента по имени\n"
           "5. Добавить абонента в текстовый файл\n"
-          "6. Удалить абонента из текстового файла\n"
-          "7. Завершить работу программы\n")
+          "6. Изменить данные абонента\n"
+          "7. Удалить абонента из текстового файла\n"
+          "8. Завершить работу программы\n")
     choice = input('> ')
     if choice.isdigit(): return int(choice)
     return show_menu()
@@ -30,7 +20,7 @@ def show_menu():
 def go_to_function(variant):
     '''Функция адресации выполнения задач
     '''
-    while (variant != 7):
+    while (variant != 8):
         if variant == 1:
             for line in read_from_txt(filename):
                 print(*line, end='')
@@ -52,6 +42,12 @@ def go_to_function(variant):
             print('\nданные внесены !', end='')
             pause()
         elif variant == 6:
+            for line in read_from_txt(filename):
+                print(*line, end='')
+            finder = input_finder('\n\nданные какого абонента вы ходите изменить?\nВведите его фамилию')
+            redact_txt(filename, finder)
+            pause()
+        elif variant == 7:
             finder = input_finder('введите фамилию или имя абонента')
             del_from_txt(filename, finder)
             print('\nданные удалены !', end='')
@@ -68,7 +64,7 @@ def read_from_txt(filename):
     return phone_list
 
 def write_to_txt(filename, phone_dict):
-    '''Функция экспорта списка в txt файл
+    '''Функция экспорта списка abon_list в txt файл
     '''
     with open(filename, 'w', encoding='utf-8') as file:
         for line in phone_dict:
@@ -92,6 +88,31 @@ def del_from_txt(filename, find_param):
             print(*line)
             phone_book.remove(line)
     write_to_txt(filename, phone_book)
+
+def redact_txt(filename, find_param):
+    '''Функция замены данных абонента в txt файле
+    '''
+    list_word = ['фамилию', 'имя', 'отчество', 'номер телефона']
+    print('какие старые данные необходимо изменить:')
+    for word in list_word:
+        print(f'{list_word.index(word) + 1} - {word}')
+    ind = (input('введите соответствующее число > '))
+    if ind.isdigit():
+        ind = int(ind)
+        if 1 <= ind <= 4:
+            new_data = input(f'Теперь новое значение - введите {list_word[ind-1]} > ').title()
+            flag = False # Проверка наличия искомого абонента
+            phone_book = read_from_txt(filename)[1:]
+            for line in phone_book:
+                if find_param in line:
+                    flag = True
+                    line[ind-1] = new_data
+                    print(*line)
+            if flag:
+                write_to_txt(filename, phone_book)
+                print('\nизменения внесены !', end='')
+                return
+    print("Вы ввели неверное значение!", end='')
 
 def input_finder(message):
     '''Функция запроса данных от пользователя
@@ -119,6 +140,7 @@ def find_abonent(filename, find_param):
         for line in find_list:
             print(*line, end='')
         return find_list
+
 
 filename = './Seminar8/phone.txt'
 fields = ["Фамилия", "Имя", "Отчество", "Телефон\n\n"]
